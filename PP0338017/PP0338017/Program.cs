43b50ffd7d374453c15aa957362cdbfeb18838c6
Rodrigo Arnaldo de Araújo <rodrigo.a.araujo@hotmail.com>
2016-09-06 13:19:09 -0300

    Add files via upload

---
+++ PP0338017/PP0338017/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP0338017
+{
+	class Program
+	{
+
+
+
+		static void Main(string[] args)
+		{
+			int cont = 0;
+			int numeroUsr;
+			int soma = 0;
+			
+
+			for (int i = 0; i <= 9; i++)
+			{
+				Console.WriteLine("Por favor, digite um número: ");
+				numeroUsr = int.Parse(Console.ReadLine());
+
+				if (numeroUsr % 2 != 0)
+				{
+					cont++;
+					soma = soma + numeroUsr;
+					
+				}
+				
+			}
+
+			Console.WriteLine("O resultado das soma é: " + soma);
+			Console.WriteLine("A quantidade de números impares são: " + cont);
+
+			Console.ReadKey();
+
+		}
+
+
+	}
+}
